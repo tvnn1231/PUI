2adb5438738475264d86a2c2856521d034b5f61d
Thuy-Vy Nguyen <thuyvyn@andrew.cmu.edu>
2020-12-03 15:46:24 -0500

    Delayed back button display

--- Homework_8/script.js
+++ Homework_8/script.js
@@ -67,22 +67,22 @@ lightbulb.onclick = function(){
 let suntl = gsap.timeline({paused: true});
 
 suntl
-.to(initialHomeBody, 2, {css: {backgroundColor: "#FFFFFF"}}, 0)
-.to(shape1, 2, {css: {x: "+=120.2245", y: "+=248.299", rotation: "+=74.19", transformOrigin: "50%, 50%", fill: "#FFD976"}}, 0)
-.to(shape2, 2, {css: {x: "+=112.493", y: "+=230.113", rotation: "+=106.79", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.9")
-.to(shape3, 2, {css: {x: "-=101.2405", y: "+=231.475", rotation: "-=106.13", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.9")
-.to(shape4, 2, {css: {x: "-=319.989", y: "+=175.807", rotation: "-=69.35", transformOrigin: "50%, 50%", fill: "#FFD976"}}, "-=1.9")
-.to(shape5, 2, {css: {x: "+=5", y: "+=241.785", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.9")
-.to(shape6, 2, {css: {x: "+=227", y: "+=128.785", rotation: "-=180", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.9")
-.to(shape7, 2, {css: {x: "+=238.155", y: "+=223.7085", rotation: "+=128.12", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.9")
-.to(shape8, 2, {css: {x: "-=220.022", y: "-=88.809", rotation: "+=311.4", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.9")
-.to(shape9, 2, {css: {x: "-=76.795", y: "+=235.004", rotation: "-=124.51", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.9")
-.to(shape10, 2, {css: {x: "+=89.797", y: "-=102.6615", rotation: "+=29.37", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.9")
-.to(shape11, 2, {css: {x: "-=5.0475", y: "-=174.21", rotation: "+=155.94", transformOrigin: "50%, 50%", fill: "#FFD976"}}, "-=1.9")
-.to(shape12, 2, {css: {x: "+=29.1265", y: "+=233.403", rotation: "+=357.87", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.9")
-.to(shape13, 2, {css: {x: "+=56.3398", y: "-=257.8546", rotation: "-=68.33", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.9")
-.to(shape14, 2, {css: {x: "-=51.6861", y: "+=83.1134", rotation: "-=68.36", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.9")
-.to(shape15, 2, {css: {x: "+=207.8748", y: "-=53.589", rotation: "+=14.86", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.9")
+.to(initialHomeBody, 1.5, {css: {backgroundColor: "#FFFFFF"}}, 0)
+.to(shape1, 1.5, {css: {x: "+=120.2245", y: "+=248.299", rotation: "+=74.19", transformOrigin: "50%, 50%", fill: "#FFD976"}}, 0)
+.to(shape2, 1.5, {css: {x: "+=112.493", y: "+=230.113", rotation: "+=106.79", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.4")
+.to(shape3, 1.5, {css: {x: "-=101.2405", y: "+=231.475", rotation: "-=106.13", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.4")
+.to(shape4, 1.5, {css: {x: "-=319.989", y: "+=175.807", rotation: "-=69.35", transformOrigin: "50%, 50%", fill: "#FFD976"}}, "-=1.4")
+.to(shape5, 1.5, {css: {x: "+=5", y: "+=241.785", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.4")
+.to(shape6, 1.5, {css: {x: "+=227", y: "+=128.785", rotation: "-=180", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.4")
+.to(shape7, 1.5, {css: {x: "+=238.155", y: "+=223.7085", rotation: "+=128.12", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.4")
+.to(shape8, 1.5, {css: {x: "-=220.022", y: "-=88.809", rotation: "+=311.4", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.4")
+.to(shape9, 1.5, {css: {x: "-=76.795", y: "+=235.004", rotation: "-=124.51", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.4")
+.to(shape10, 1.5, {css: {x: "+=89.797", y: "-=102.6615", rotation: "+=29.37", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.4")
+.to(shape11, 1.5, {css: {x: "-=5.0475", y: "-=174.21", rotation: "+=155.94", transformOrigin: "50%, 50%", fill: "#FFD976"}}, "-=1.4")
+.to(shape12, 1.5, {css: {x: "+=29.1265", y: "+=233.403", rotation: "+=357.87", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.4")
+.to(shape13, 1.5, {css: {x: "+=56.3398", y: "-=257.8546", rotation: "-=68.33", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.4")
+.to(shape14, 1.5, {css: {x: "-=51.6861", y: "+=83.1134", rotation: "-=68.36", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.4")
+.to(shape15, 1.5, {css: {x: "+=207.8748", y: "-=53.589", rotation: "+=14.86", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.4")
 
 // 1y, 2xy, 3xy, 7y, 8xy, 9y, 10xy 
 
@@ -109,9 +109,20 @@ function setModalAttributes() {
 
 // display back button
 let backbtntl = gsap.timeline({paused: true});
-
 backbtntl
-.to(backBtn, 1, {css: {opacity: 1}}, 0)
+.to(backBtn, 1.5, {css: {opacity: 1}}, 0);
+
+function showBackBtn() {
+    backBtn.style.display = "block";
+    backbtntl.play();
+    if (lightbulb.getAttribute("picture") === "sun") {
+        backBtn.classList.add("sun-back-btn");
+    } else if (lightbulb.getAttribute("picture") === "wind") {
+        backBtn.classList.add("wind-back-btn");
+    } else if (lightbulb.getAttribute("picture") === "water") {
+        backBtn.classList.add("water-back-btn");                // DEBUG: button was gray not blue, had backbtn instead of backBtn (used inspect to find error "backbtn not defined")
+    }
+}
 
 for (i = 0;i < sunClick.length; i += 1) {
     sunClick[i].onclick = function() {
@@ -136,10 +147,8 @@ for (i = 0;i < sunClick.length; i += 1) {
             // prevent pipes from showing up
             lightbulb.onclick = hidePipes();
 
-            // show back button
-            backBtn.style.display = "block";
-            backbtntl.play();
-            backBtn.classList.add("sun-back-btn");
+            // show back button after 1 second (so doesn't distract from lightbulb animation)
+            setTimeout(showBackBtn, 1000);
         }
     }
 }
@@ -148,22 +157,22 @@ for (i = 0;i < sunClick.length; i += 1) {
 let windtl = gsap.timeline({paused: true});
 
 windtl
-.to(initialHomeBody, 2, {css: {backgroundColor: "#FFFFFF"}}, 0)
-.to(shape1, 2, {css: {x: "-=492.2415", y: "-=10.5335", rotation: "+=74.13", transformOrigin: "50%, 50%", fill: "#0C9612"}}, 0)
-.to(shape2, 2, {css: {x: "+=670.4012", y: "+=224.249", rotation: "+=51.5", transformOrigin: "50%, 50%", fill: "#09551C"}}, "-=1.9")
-.to(shape3, 2, {css: {x: "-=331.6858", y: "+=276.3445", rotation: "+=134.93", transformOrigin: "50%, 50%", fill: "#0C9612"}}, "-=1.9")
-.to(shape4, 2, {css: {x: "-=253.7858", y: "+=36.844", rotation: "-=83.97", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=1.9")
-.to(shape5, 2, {css: {x: "+=196.7602", y: "+=218.959", rotation: "+=40.54", transformOrigin: "50%, 50%", fill: "#E5E5E5"}}, "-=1.9")
-.to(shape6, 2, {css: {x: "+=36.8012", y: "-=87.0425", rotation: "+=18.66", transformOrigin: "50%, 50%", fill: "#0C9612"}}, "-=1.9")
-.to(shape7, 2, {css: {x: "+=334.7462", y: "+=122.304", rotation: "+=117.66", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.9")
-.to(shape8, 2, {css: {x: "+=150.4717", y: "+=385.676", rotation: "+=196.05", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.9")
-.to(shape9, 2, {css: {x: "+=158.4067", y: "+=60.004", rotation: "-=73.67", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.9")
-.to(shape10, 2, {css: {x: "+=130.3247", y: "-=76.125", rotation: "-=1.58", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.9")
-.to(shape11, 2, {css: {x: "+=201.8997", y: "+=319.889", rotation: "-=172.93", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.9")
-.to(shape12, 2, {css: {x: "+=174.8332", y: "+=124.1895", rotation: "+=7.22", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.9")
-.to(shape13, 2, {css: {x: "+=496.0453", y: "-=259.2026", rotation: "+=17.87", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=1.9")
-.to(shape14, 2, {css: {x: "-=417.581", y: "-=236.8879", rotation: "+=25.57", transformOrigin: "50%, 50%", fill: "#09551C"}}, "-=1.9")
-.to(shape15, 2, {css: {x: "-=620.3759", y: "-=113.5079", rotation: "-=24.79", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=1.9")
+.to(initialHomeBody, 1.5, {css: {backgroundColor: "#FFFFFF"}}, 0)
+.to(shape1, 1.5, {css: {x: "-=492.2415", y: "-=10.5335", rotation: "+=74.13", transformOrigin: "50%, 50%", fill: "#0C9612"}}, 0)
+.to(shape2, 1.5, {css: {x: "+=670.4012", y: "+=224.249", rotation: "+=51.5", transformOrigin: "50%, 50%", fill: "#09551C"}}, "-=1.4")
+.to(shape3, 1.5, {css: {x: "-=331.6858", y: "+=276.3445", rotation: "+=134.93", transformOrigin: "50%, 50%", fill: "#0C9612"}}, "-=1.4")
+.to(shape4, 1.5, {css: {x: "-=253.7858", y: "+=36.844", rotation: "-=83.97", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=1.4")
+.to(shape5, 1.5, {css: {x: "+=196.7602", y: "+=218.959", rotation: "+=40.54", transformOrigin: "50%, 50%", fill: "#E5E5E5"}}, "-=1.4")
+.to(shape6, 1.5, {css: {x: "+=36.8012", y: "-=87.0425", rotation: "+=18.66", transformOrigin: "50%, 50%", fill: "#0C9612"}}, "-=1.4")
+.to(shape7, 1.5, {css: {x: "+=334.7462", y: "+=122.304", rotation: "+=117.66", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
+.to(shape8, 1.5, {css: {x: "+=150.4717", y: "+=385.676", rotation: "+=196.05", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
+.to(shape9, 1.5, {css: {x: "+=158.4067", y: "+=60.004", rotation: "-=73.67", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
+.to(shape10, 1.5, {css: {x: "+=130.3247", y: "-=76.125", rotation: "-=1.58", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
+.to(shape11, 1.5, {css: {x: "+=201.8997", y: "+=319.889", rotation: "-=172.93", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
+.to(shape12, 1.5, {css: {x: "+=174.8332", y: "+=124.1895", rotation: "+=7.22", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
+.to(shape13, 1.5, {css: {x: "+=496.0453", y: "-=259.2026", rotation: "+=17.87", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=1.4")
+.to(shape14, 1.5, {css: {x: "-=417.581", y: "-=236.8879", rotation: "+=25.57", transformOrigin: "50%, 50%", fill: "#09551C"}}, "-=1.4")
+.to(shape15, 1.5, {css: {x: "-=620.3759", y: "-=113.5079", rotation: "-=24.79", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=1.4")
 
 // 5xy, 7xy, 8xy, 9xy, 11xy
 
@@ -189,13 +198,12 @@ for (i = 0; i < windClick.length; i += 1) {
             id = "#wind9";
             setModalAttributes();
             
+            // DEBUG: had this outside if statement, needed to be inside for pipes not to show up after clicking (worked for sun bc had set up modals already)
             // prevent pipes from showing up
             lightbulb.onclick = hidePipes();
 
-            // show back button
-            backBtn.style.display = "block";
-            backbtntl.play();
-            backBtn.classList.add("wind-back-btn");
+            // show back button after 1 second
+            setTimeout(showBackBtn, 1000);
         }
     }
 }
@@ -238,22 +246,22 @@ for (i = 0; i < windClick.length; i += 1) {
 let watertl = gsap.timeline({paused: true});
 
 watertl
-.to(initialHomeBody, 2, {css: {backgroundColor: "#FFFFFF"}}, 0)
-.to(shape1, 2, {css: {x: "+=252.525", y: "+=434.959", rotation: "+=146.46", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, 0)
-.to(shape2, 2, {css: {x: "+=173.251", y: "+=591.9025", rotation: "+=149.4", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=1.9")
-.to(shape3, 2, {css: {x: "-=95.9055", y: "+=591.9025", rotation: "-=148.76", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=1.9")
-.to(shape4, 2, {css: {x: "-=175.518", y: "+=434.959", rotation: "-=145.83", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=1.9")
-.to(shape5, 2, {css: {x: "+=40.06", y: "+=354", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.9")
-.to(shape6, 2, {css: {x: "+=39", y: "+=356.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.9")
-.to(shape7, 2, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.9")
-.to(shape8, 2, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.9")
-.to(shape9, 2, {css: {x: "-=14.951", y: "-=15.175", rotation: "-=172.82", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=1.9")
-.to(shape10, 2, {css: {x: "-=291.696", y: "-=15.0055", rotation: "-=186.81", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=1.9")
-.to(shape11, 2, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.9")
-.to(shape12, 2, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.9")
-.to(shape13, 2, {css: {x: "+=292", y: "-=54", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.9")
-.to(shape14, 2, {css: {x: "-=288", y: "+=171", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.9")
-.to(shape15, 2, {css: {x: "+=130", y: "-=363", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=1.9")
+.to(initialHomeBody, 1.5, {css: {backgroundColor: "#FFFFFF"}}, 0)
+.to(shape1, 1.5, {css: {x: "+=252.525", y: "+=434.959", rotation: "+=146.46", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, 0)
+.to(shape2, 1.5, {css: {x: "+=173.251", y: "+=591.9025", rotation: "+=149.4", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=1.4")
+.to(shape3, 1.5, {css: {x: "-=95.9055", y: "+=591.9025", rotation: "-=148.76", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=1.4")
+.to(shape4, 1.5, {css: {x: "-=175.518", y: "+=434.959", rotation: "-=145.83", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=1.4")
+.to(shape5, 1.5, {css: {x: "+=40.06", y: "+=354", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.4")
+.to(shape6, 1.5, {css: {x: "+=39", y: "+=356.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.4")
+.to(shape7, 1.5, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.4")
+.to(shape8, 1.5, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.4")
+.to(shape9, 1.5, {css: {x: "-=14.951", y: "-=15.175", rotation: "-=172.82", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=1.4")
+.to(shape10, 1.5, {css: {x: "-=291.696", y: "-=15.0055", rotation: "-=186.81", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=1.4")
+.to(shape11, 1.5, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.4")
+.to(shape12, 1.5, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.4")
+.to(shape13, 1.5, {css: {x: "+=292", y: "-=54", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.4")
+.to(shape14, 1.5, {css: {x: "-=288", y: "+=171", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.4")
+.to(shape15, 1.5, {css: {x: "+=130", y: "-=363", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=1.4")
 
 // 1xy, 2xy, 3xy, 4xy, 9x
 
@@ -280,10 +288,8 @@ for (i = 0; i < waterClick.length; i += 1) {
             // prevent pipes from showing up
             lightbulb.onclick = hidePipes();
 
-            // show back button
-            backBtn.style.display = "block";
-            backbtntl.play();
-            backBtn.classList.add("water-back-btn");
+            // show back button after 1 second
+            setTimeout(showBackBtn, 1000);
         }
     }
 }
@@ -324,8 +330,11 @@ backBtn.onclick = function() {
     suntl.reverse();
     windtl.reverse();
     watertl.reverse();
-    setTimeout(showPipes, 3500)                 // delays pipes re-displaying for 3.5 seconds
+    setTimeout(showPipes, 3000)                 // delays pipes re-displaying for 3 seconds
     backbtntl.reverse();
+    setTimeout(function() {                     // so color stays until button disappears
+        backBtn.classList.remove("sun-back-btn", "wind-back-btn", "water-back-btn");
+    }, 3000)
     // setTimeout(function() {                 
     //     mainPipes.style.display = "block";
     // }, 3500);
