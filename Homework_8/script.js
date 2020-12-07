0f85b2963d6cc2fbdbd51384e5aac27664cfa4c2
Thuy-Vy Nguyen <thuyvyn@andrew.cmu.edu>
2020-12-07 17:01:00 -0500

    Condensed border animation code and added to correct shapes for each energy

--- Homework_8/script.js
+++ Homework_8/script.js
@@ -45,7 +45,7 @@ let waterSum = document.getElementById("water-summary");
 let lightbulbtl = gsap.timeline({paused: true});
 
 lightbulbtl
-.to(lightbulb, 1, {x: "+=0", y: "-=190", scale: 1.2, transformOrigin: "50%, 50%"}, 0)
+.to(lightbulb, 1, {x: "+=0", y: "-=200", scale: 1.2, transformOrigin: "50%, 50%"}, 0)
 .to(initialH1, 1, {css: {opacity: 0}}, 0)
 .to(initialP, 1, {css: {opacity: 0}}, 0)
 .to(clickOnLightbulb, 1, {css: {opacity: 0}}, 0)
@@ -159,22 +159,22 @@ function showSum() {
 let suntl = gsap.timeline({paused: true});
 
 suntl
-.to(initialHomeBody, 1.5, {css: {backgroundColor: "#FFFFFF"}}, 0)
-.to(shape1, 1.5, {css: {x: "+=120.2245", y: "+=248.299", rotation: "+=74.19", transformOrigin: "50%, 50%", fill: "#FFD976"}}, 0)
-.to(shape2, 1.5, {css: {x: "+=112.493", y: "+=230.113", rotation: "+=106.79", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.4")
-.to(shape3, 1.5, {css: {x: "-=101.2405", y: "+=231.475", rotation: "-=106.13", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.4")
-.to(shape4, 1.5, {css: {x: "-=319.989", y: "+=175.807", rotation: "-=69.35", transformOrigin: "50%, 50%", fill: "#FFD976"}}, "-=1.4")
-.to(shape5, 1.5, {css: {x: "+=5", y: "+=241.785", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.4")
-.to(shape6, 1.5, {css: {x: "+=227", y: "+=128.785", rotation: "-=180", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.4")
-.to(shape7, 1.5, {css: {x: "+=238.155", y: "+=223.7085", rotation: "+=128.12", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.4")
-.to(shape8, 1.5, {css: {x: "-=220.022", y: "-=88.809", rotation: "+=311.4", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.4")
-.to(shape9, 1.5, {css: {x: "-=76.795", y: "+=235.004", rotation: "-=124.51", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.4")
-.to(shape10, 1.5, {css: {x: "+=89.797", y: "-=102.6615", rotation: "+=29.37", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.4")
-.to(shape11, 1.5, {css: {x: "-=5.0475", y: "-=174.21", rotation: "+=155.94", transformOrigin: "50%, 50%", fill: "#FFD976"}}, "-=1.4")
-.to(shape12, 1.5, {css: {x: "+=29.1265", y: "+=233.403", rotation: "+=357.87", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.4")
-.to(shape13, 1.5, {css: {x: "+=56.3398", y: "-=257.8546", rotation: "-=68.33", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=1.4")
-.to(shape14, 1.5, {css: {x: "-=51.6861", y: "+=83.1134", rotation: "-=68.36", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=1.4")
-.to(shape15, 1.5, {css: {x: "+=207.8748", y: "-=53.589", rotation: "+=14.86", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=1.4")
+.to(initialHomeBody, 1, {ease: "circ.easeOut", css: {backgroundColor: "#FFFFFF"}}, 0)
+.to(shape1, 1, {ease: "circ.easeOut", css: {x: "+=120.2245", y: "+=248.299", rotation: "+=74.19", transformOrigin: "50%, 50%", fill: "#FFD976"}}, 0)
+.to(shape2, 1, {ease: "circ.easeOut", css: {x: "+=112.493", y: "+=230.113", rotation: "+=106.79", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=0.9")
+.to(shape3, 1, {ease: "circ.easeOut", css: {x: "-=101.2405", y: "+=231.475", rotation: "-=106.13", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=0.9")
+.to(shape4, 1, {ease: "circ.easeOut", css: {x: "-=319.989", y: "+=175.807", rotation: "-=69.35", transformOrigin: "50%, 50%", fill: "#FFD976"}}, "-=0.9")
+.to(shape5, 1, {ease: "circ.easeOut", css: {x: "+=5", y: "+=241.785", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=0.9")
+.to(shape6, 1, {ease: "circ.easeOut", css: {x: "+=227", y: "+=128.785", rotation: "-=180", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=0.9")
+.to(shape7, 1, {ease: "circ.easeOut", css: {x: "+=238.155", y: "+=223.7085", rotation: "+=128.12", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=0.9")
+.to(shape8, 1, {ease: "circ.easeOut", css: {x: "-=220.022", y: "-=88.809", rotation: "+=311.4", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=0.9")
+.to(shape9, 1, {ease: "circ.easeOut", css: {x: "-=76.795", y: "+=235.004", rotation: "-=124.51", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=0.9")
+.to(shape10, 1, {ease: "circ.easeOut", css: {x: "+=89.797", y: "-=102.6615", rotation: "+=29.37", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=0.9")
+.to(shape11, 1, {ease: "circ.easeOut", css: {x: "-=5.0475", y: "-=174.21", rotation: "+=155.94", transformOrigin: "50%, 50%", fill: "#FFD976"}}, "-=0.9")
+.to(shape12, 1, {ease: "circ.easeOut", css: {x: "+=29.1265", y: "+=233.403", rotation: "+=357.87", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=0.9")
+.to(shape13, 1, {ease: "circ.easeOut", css: {x: "+=56.3398", y: "-=257.8546", rotation: "-=68.33", transformOrigin: "50%, 50%", fill: "#FC9700"}}, "-=0.9")
+.to(shape14, 1, {ease: "circ.easeOut", css: {x: "-=51.6861", y: "+=83.1134", rotation: "-=68.36", transformOrigin: "50%, 50%", fill: "#FFC700"}}, "-=0.9")
+.to(shape15, 1, {ease: "circ.easeOut", css: {x: "+=207.8748", y: "-=53.589", rotation: "+=14.86", transformOrigin: "50%, 50%", fill: "#FFF500"}}, "-=0.9")
 
 // 1y, 2xy, 3xy, 7y, 8xy, 9y, 10xy 
 
@@ -191,36 +191,24 @@ function viewport900() {
     lightbulb.style.height = "540px";
 }
 
-
+let sunShapes = document.getElementsByClassName("sun-shape");
+let windShapes = document.getElementsByClassName("wind-shape");
+let waterShapes = document.getElementsByClassName("water-shape");
 
 for (i = 0; i < sunClick.length; i += 1) {
     sunClick[i].onclick = function() {
         suntl.play();
         lightbulb.setAttribute("picture", "sun");
         if (lightbulb.getAttribute("picture") === "sun") {
-    
-            // set modal attributes for shapes 7, 8, 9, and 10
-            shape = shape8;
-            id = "#sun8";
-            setModalAttributes();
-            shape.classList.add("sun-glow")
 
-            shape = shape10;
-            id = "#sun10";
-            setModalAttributes();
-            shape.classList.add("sun-glow");
-
-            shape = shape7;
-            id = "#sun7";
-            setModalAttributes();
-            shape.classList.add("sun-glow");
-
-
-            shape = shape9;
-            id = "#sun9";
-            setModalAttributes();
-            shape.classList.add("sun-glow");
-
+            // set modals and show clickable
+            for (i = 0; i < sunShapes.length; i += 1) {
+                shape = sunShapes[i];
+                let str = sunShapes[i].id;
+                id = "#sun" + str.slice(5);
+                setModalAttributes();
+                shape.classList.add("sun-glow");
+            }
             
             // prevent pipes from showing up
             lightbulb.onclick = hidePipes();
@@ -237,22 +225,22 @@ for (i = 0; i < sunClick.length; i += 1) {
 let windtl = gsap.timeline({paused: true});
 
 windtl
-.to(initialHomeBody, 1.5, {css: {backgroundColor: "#FFFFFF"}}, 0)
-.to(shape1, 1.5, {css: {x: "-=492.2415", y: "-=10.5335", rotation: "+=74.13", transformOrigin: "50%, 50%", fill: "#0C9612"}}, 0)
-.to(shape2, 1.5, {css: {x: "+=670.4012", y: "+=224.249", rotation: "+=51.5", transformOrigin: "50%, 50%", fill: "#09551C"}}, "-=1.4")
-.to(shape3, 1.5, {css: {x: "-=331.6858", y: "+=276.3445", rotation: "+=134.93", transformOrigin: "50%, 50%", fill: "#0C9612"}}, "-=1.4")
-.to(shape4, 1.5, {css: {x: "-=253.7858", y: "+=36.844", rotation: "-=83.97", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=1.4")
-.to(shape5, 1.5, {css: {x: "+=196.7602", y: "+=218.959", rotation: "+=40.54", transformOrigin: "50%, 50%", fill: "#E5E5E5"}}, "-=1.4")
-.to(shape6, 1.5, {css: {x: "+=36.8012", y: "-=87.0425", rotation: "+=18.66", transformOrigin: "50%, 50%", fill: "#0C9612"}}, "-=1.4")
-.to(shape7, 1.5, {css: {x: "+=334.7462", y: "+=122.304", rotation: "+=117.66", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
-.to(shape8, 1.5, {css: {x: "+=150.4717", y: "+=385.676", rotation: "+=196.05", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
-.to(shape9, 1.5, {css: {x: "+=158.4067", y: "+=60.004", rotation: "-=73.67", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
-.to(shape10, 1.5, {css: {x: "+=130.3247", y: "-=76.125", rotation: "-=1.58", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
-.to(shape11, 1.5, {css: {x: "+=201.8997", y: "+=319.889", rotation: "-=172.93", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
-.to(shape12, 1.5, {css: {x: "+=174.8332", y: "+=124.1895", rotation: "+=7.22", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=1.4")
-.to(shape13, 1.5, {css: {x: "+=496.0453", y: "-=259.2026", rotation: "+=17.87", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=1.4")
-.to(shape14, 1.5, {css: {x: "-=417.581", y: "-=236.8879", rotation: "+=25.57", transformOrigin: "50%, 50%", fill: "#09551C"}}, "-=1.4")
-.to(shape15, 1.5, {css: {x: "-=620.3759", y: "-=113.5079", rotation: "-=24.79", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=1.4")
+.to(initialHomeBody, 1, {ease: "circ.easeOut", css: {backgroundColor: "#FFFFFF"}}, 0)
+.to(shape1, 1, {ease: "circ.easeOut", css: {x: "-=492.2415", y: "-=10.5335", rotation: "+=74.13", transformOrigin: "50%, 50%", fill: "#0C9612"}}, 0)
+.to(shape2, 1, {ease: "circ.easeOut", css: {x: "+=670.4012", y: "+=224.249", rotation: "+=51.5", transformOrigin: "50%, 50%", fill: "#09551C"}}, "-=0.9")
+.to(shape3, 1, {ease: "circ.easeOut", css: {x: "-=331.6858", y: "+=276.3445", rotation: "+=134.93", transformOrigin: "50%, 50%", fill: "#0C9612"}}, "-=0.9")
+.to(shape4, 1, {ease: "circ.easeOut", css: {x: "-=253.7858", y: "+=36.844", rotation: "-=83.97", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=0.9")
+.to(shape5, 1, {ease: "circ.easeOut", css: {x: "+=196.7602", y: "+=218.959", rotation: "+=40.54", transformOrigin: "50%, 50%", fill: "#E5E5E5"}}, "-=0.9")
+.to(shape6, 1, {ease: "circ.easeOut", css: {x: "+=36.8012", y: "-=87.0425", rotation: "+=18.66", transformOrigin: "50%, 50%", fill: "#0C9612"}}, "-=0.9")
+.to(shape7, 1, {ease: "circ.easeOut", css: {x: "+=334.7462", y: "+=122.304", rotation: "+=117.66", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=0.9")
+.to(shape8, 1, {ease: "circ.easeOut", css: {x: "+=150.4717", y: "+=385.676", rotation: "+=196.05", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=0.9")
+.to(shape9, 1, {ease: "circ.easeOut", css: {x: "+=158.4067", y: "+=60.004", rotation: "-=73.67", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=0.9")
+.to(shape10, 1, {ease: "circ.easeOut", css: {x: "+=130.3247", y: "-=76.125", rotation: "-=1.58", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=0.9")
+.to(shape11, 1, {ease: "circ.easeOut", css: {x: "+=201.8997", y: "+=319.889", rotation: "-=172.93", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=0.9")
+.to(shape12, 1, {ease: "circ.easeOut", css: {x: "+=174.8332", y: "+=124.1895", rotation: "+=7.22", transformOrigin: "50%, 50%", fill: "#FFFFFF"}}, "-=0.9")
+.to(shape13, 1, {ease: "circ.easeOut", css: {x: "+=496.0453", y: "-=259.2026", rotation: "+=17.87", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=0.9")
+.to(shape14, 1, {ease: "circ.easeOut", css: {x: "-=417.581", y: "-=236.8879", rotation: "+=25.57", transformOrigin: "50%, 50%", fill: "#09551C"}}, "-=0.9")
+.to(shape15, 1, {ease: "circ.easeOut", css: {x: "-=620.3759", y: "-=113.5079", rotation: "-=24.79", transformOrigin: "50%, 50%", fill: "#71B637"}}, "-=0.9")
 
 // 5xy, 7xy, 8xy, 9xy, 11xy
 
@@ -262,31 +250,17 @@ for (i = 0; i < windClick.length; i += 1) {
         windtl.play();
         lightbulb.style.width = "100%";         // so sides don't get cut off
         lightbulb.setAttribute("picture", "wind");
+
         if (lightbulb.getAttribute("picture") === "wind") {
-    
-            // set modal attributes for shapes 7, 8, 9, and 10
-            shape = shape8;
-            id = "#wind8";
-            setModalAttributes();
-            shape.classList.add("wind-glow");
 
-
-            shape = shape10;
-            id = "#wind10";
-            setModalAttributes();
-            shape.classList.add("wind-glow");
-
-
-            shape = shape7;
-            id = "#wind7";
-            setModalAttributes();
-            shape.classList.add("wind-glow");
-
-
-            shape = shape9;
-            id = "#wind9";
-            setModalAttributes();
-            shape.classList.add("wind-glow");
+            // set modals and show clickable
+            for (i = 0; i < windShapes.length; i += 1) {
+                shape = windShapes[i];
+                let str = windShapes[i].id;
+                id = "#wind" + str.slice(5);
+                setModalAttributes();
+                shape.classList.add("wind-glow");
+            }
 
             
             // DEBUG: had this outside if statement, needed to be inside for pipes not to show up after clicking (worked for sun bc had set up modals already)
@@ -339,22 +313,22 @@ for (i = 0; i < windClick.length; i += 1) {
 let watertl = gsap.timeline({paused: true});
 
 watertl
-.to(initialHomeBody, 1.5, {css: {backgroundColor: "#FFFFFF"}}, 0)
-.to(shape1, 1.5, {css: {x: "+=252.525", y: "+=434.959", rotation: "+=146.46", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, 0)
-.to(shape2, 1.5, {css: {x: "+=173.251", y: "+=591.9025", rotation: "+=149.4", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=1.4")
-.to(shape3, 1.5, {css: {x: "-=95.9055", y: "+=591.9025", rotation: "-=148.76", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=1.4")
-.to(shape4, 1.5, {css: {x: "-=175.518", y: "+=434.959", rotation: "-=145.83", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=1.4")
-.to(shape5, 1.5, {css: {x: "+=40.06", y: "+=354", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.4")
-.to(shape6, 1.5, {css: {x: "+=39", y: "+=356.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.4")
-.to(shape7, 1.5, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.4")
-.to(shape8, 1.5, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.4")
-.to(shape9, 1.5, {css: {x: "-=14.951", y: "-=15.175", rotation: "-=172.82", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=1.4")
-.to(shape10, 1.5, {css: {x: "-=291.696", y: "-=15.0055", rotation: "-=186.81", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=1.4")
-.to(shape11, 1.5, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.4")
-.to(shape12, 1.5, {css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.4")
-.to(shape13, 1.5, {css: {x: "+=292", y: "-=54", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=1.4")
-.to(shape14, 1.5, {css: {x: "-=288", y: "+=171", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=1.4")
-.to(shape15, 1.5, {css: {x: "+=130", y: "-=363", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=1.4")
+.to(initialHomeBody, 1, {ease: "circ.easeOut", css: {backgroundColor: "#FFFFFF"}}, 0)
+.to(shape1, 1, {ease: "circ.easeOut", css: {x: "+=252.525", y: "+=434.959", rotation: "+=146.46", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, 0)
+.to(shape2, 1, {ease: "circ.easeOut", css: {x: "+=173.251", y: "+=591.9025", rotation: "+=149.4", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=0.9")
+.to(shape3, 1, {ease: "circ.easeOut", css: {x: "-=95.9055", y: "+=591.9025", rotation: "-=148.76", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=0.9")
+.to(shape4, 1, {ease: "circ.easeOut", css: {x: "-=175.518", y: "+=434.959", rotation: "-=145.83", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=0.9")
+.to(shape5, 1, {ease: "circ.easeOut", css: {x: "+=40.06", y: "+=354", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=0.9")
+.to(shape6, 1, {ease: "circ.easeOut", css: {x: "+=39", y: "+=356.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=0.9")
+.to(shape7, 1, {ease: "circ.easeOut", css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=0.9")
+.to(shape8, 1, {ease: "circ.easeOut", css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=0.9")
+.to(shape9, 1, {ease: "circ.easeOut", css: {x: "-=14.951", y: "-=15.175", rotation: "-=172.82", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=0.9")
+.to(shape10, 1, {ease: "circ.easeOut", css: {x: "-=291.696", y: "-=15.0055", rotation: "-=186.81", transformOrigin: "50%, 50%", fill: "#1313EF"}}, "-=0.9")
+.to(shape11, 1, {ease: "circ.easeOut", css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=0.9")
+.to(shape12, 1, {ease: "circ.easeOut", css: {x: "+=39", y: "+=353.44", rotation: "+=0", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=0.9")
+.to(shape13, 1, {ease: "circ.easeOut", css: {x: "+=292", y: "-=54", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#0085FF"}}, "-=0.9")
+.to(shape14, 1, {ease: "circ.easeOut", css: {x: "-=288", y: "+=171", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#46A6FF"}}, "-=0.9")
+.to(shape15, 1, {ease: "circ.easeOut", css: {x: "+=130", y: "-=363", rotation: "-=90", transformOrigin: "50%, 50%", fill: "#1C5BFF"}}, "-=0.9")
 
 // 1xy, 2xy, 3xy, 4xy, 9x
 
@@ -363,32 +337,15 @@ for (i = 0; i < waterClick.length; i += 1) {
         watertl.play();
         lightbulb.setAttribute("picture", "water");
         if (lightbulb.getAttribute("picture") === "water") {
-    
-            // set modal attributes for shapes 7, 8, 9, and 10
-            shape = shape8;
-            id = "#water8";
-            setModalAttributes();
-            shape.classList.add("water-glow");
 
-
-            shape = shape10;
-            id = "#water10";
-            setModalAttributes();
-            shape.classList.add("water-glow");
-
-
-            shape = shape7;
-            id = "#water7";
-            setModalAttributes();
-            shape.classList.add("water-glow");
-
-
-            shape = shape9;
-            id = "#water9";
-            setModalAttributes();
-            shape.classList.add("water-glow");
-
-
+            // set modals and show clickable
+            for (i = 0; i < waterShapes.length; i += 1) {
+                shape = waterShapes[i];
+                let str = waterShapes[i].id;
+                id = "#water" + str.slice(5);
+                setModalAttributes();
+                shape.classList.add("water-glow");
+            }
             
             // prevent pipes from showing up
             lightbulb.onclick = hidePipes();
@@ -440,15 +397,20 @@ backBtn.onclick = function() {
     suntl.reverse();
     windtl.reverse();
     watertl.reverse();
-    setTimeout(showPipes, 3000)                 // delays pipes re-displaying for 3 seconds
+    setTimeout(showPipes, 2500)                 // delays pipes re-displaying for 2.5 seconds
     backbtntl.reverse();
     titletl.reverse();
     sumtl.reverse();
-    shape7.classList.remove("sun-glow", "wind-glow", "water-glow");
-    shape8.classList.remove("sun-glow", "wind-glow", "water-glow");
-    shape9.classList.remove("sun-glow", "wind-glow", "water-glow");
-    shape10.classList.remove("sun-glow", "wind-glow", "water-glow");
-    
+    for (i = 0; i < sunShapes.length; i += 1) {
+        sunShapes[i].classList.remove("sun-glow");
+    }
+    for (i = 0; i < windShapes.length; i += 1) {
+        windShapes[i].classList.remove("wind-glow");
+    }
+    for (i = 0; i < waterShapes.length; i += 1) {
+        waterShapes[i].classList.remove("water-glow");
+    }
+
     // DOESN'T WORK
     // for (i = 0; i < sunGlow.length; i += 1) {
     //     sunGlow[i].classList.remove("sun-glow");
